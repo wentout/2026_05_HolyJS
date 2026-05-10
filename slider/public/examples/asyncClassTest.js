d465f18b123ef68a4f218dd1c2df7f34b51f20fb
wentout <went.out@gmail.com>
2026-05-10 11:55:41 +0300

    some working state

---
+++ slider/public/examples/asyncClassTest.js
@@ -0,0 +1,37 @@
+class MyAsyncClass {
+	field = undefined;
+	constructor() {
+		const self = this;
+		return new Promise((resolve) => {
+			setTimeout(() => {
+				self.field = 123;
+				resolve(self);
+			}, 1000);
+		});
+	}
+}
+
+debugger;
+
+class MySubAsyncClass extends MyAsyncClass {
+	constructor() {
+		const promise = super();
+		return new Promise(async (resolve) => {
+			console.log('start');
+			const item = await promise;
+			console.log(item.field); // 123
+			console.log('finish');
+			// not this, but item, cause awaited
+			resolve(item);
+		});
+	}
+
+}
+
+debugger;
+
+(async () => {
+	const item = await (new MySubAsyncClass);
+	console.log(item.field); // 123
+	debugger;
+})();
